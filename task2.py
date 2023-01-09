8ce1df13c5cc40314ddfe5776d4d42c3800ed20c
nukedayls <108824847+nukedayls@users.noreply.github.com>
2023-01-09 15:42:33 +0200

    Delete task2.py

--- task2.py
+++
@@ -1,7 +0,0 @@
-def first_non_repeating_letter(string):
-    non_repeating_letter = [letter for letter in string if string.lower().count(letter.lower()) == 1]
-    return  non_repeating_letter[0] if len(non_repeating_letter) > 0 else ""
-
-print(first_non_repeating_letter('stress'))
-print(first_non_repeating_letter('sTress'))
-print(first_non_repeating_letter('ababab'))
