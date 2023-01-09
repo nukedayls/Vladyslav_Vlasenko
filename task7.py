2006998544d86490268b6d7b8b1e93a79e47c681
nukedayls <108824847+nukedayls@users.noreply.github.com>
2023-01-09 15:43:29 +0200

    Delete task7.py

--- task7.py
+++
@@ -1,7 +0,0 @@
-def int_to_ip(int32):
-    bin_str = f'{int32:b}'.rjust(32, '0')
-    return '.'.join([str(int(bin_str[idx:idx + 8], 2)) for idx in range(0, len(bin_str), 8)])
-
-print(int_to_ip(2149583361))
-print(int_to_ip(32))
-print(int_to_ip(0))
