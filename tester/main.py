c0d5ee4d089643ef237a5ad952fc5bdee48dc87a
nukedayls <108824847+nukedayls@users.noreply.github.com>
2023-01-12 18:16:13 +0200

    Homework

--- tester/main.py
+++ tester/main.py
@@ -1,1 +1,27 @@
+from test import Tester
+import config as cfg
+from fastapi import FastAPI
 
+if cfg.ACCESS_TOKEN == "":
+    print('Set Access token in "config.py" for dropbox')
+
+app = FastAPI()
+tester = Tester(cfg.ACCESS_TOKEN, 'smile.png')
+
+@app.get("/")
+def read_root():
+    return {"Status": 200}
+
+@app.post("/image")
+def upload_file():
+    return tester.upload()
+
+
+@app.get("/image/metadata")
+def file_get_metadata():
+    return tester.get_metadata()
+
+
+@app.delete("/del")
+def delete_file():
+    return tester.delete()
